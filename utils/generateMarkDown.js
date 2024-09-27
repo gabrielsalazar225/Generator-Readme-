b72c402a771eff7d76bed409f3e8ade504dd8f70
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-09-27 13:19:01 -0400

    render structure with the prompts

--- utils/generateMarkDown.js
+++ utils/generateMarkDown.js
@@ -17,3 +17,52 @@ function renderLicenseLink(license) {
       'None' : 'hole'};
       return LicenseLinks[license] || '';
 }
+
+function renderLicenseSection(license) {
+  const LicenseLinks = {
+      'MIT' : '[MIT] (https://opensource.org/license/mit)',
+      'Apache 2.0' : '[Apache 2.0] (https://opensource.org/license/apache-2-0)',
+      'GPL 3.0' : '[GPL 3.0] (https://www.gnu.org/licenses/gpl-3.0)',
+      'BSD 3' : '[BSD 3] (https://opensource.org/license/bsd-3-clause)',
+      'None' : ''};
+      if (license === 'NONE') {
+          return '';
+      } 
+      const LicenseLink = LicenseLinks[license]; 
+      return `## license
+      This project is licensed under the ${LicenseLink} license
+      `
+}
+function generateMarkdown(data) {
+
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+  
+  return `
+  # ${data.title} 
+  ${licenseBadge}
+  ## Description 
+  ${data.description}
+  ## Table content
+  - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [License](#license)
+    - [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ${licenseSection}
+  ## Questions
+  If you have any questions, you can reach me at ${data.email} or visit my GitHub profile at https://github.com/${data.username}.
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
