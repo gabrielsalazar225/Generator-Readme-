2ea7e99625f160979b7f394ec5b951a36f10062d
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-09-27 12:56:36 -0400

    inputs created in the generaMarkDown.js

--- utils/generateMarkDown.js
+++ utils/generateMarkDown.js
@@ -3,3 +3,51 @@ const generateMarkdown = require ('./utils/generateMarkdown')
 const fs = require('fs')
 const inquirer = require('inquirer')
 
+const questions  = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions for your project',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information for your project',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide contribution guidelines for your project',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions for your project',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project',
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'NONE'],
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Provide your GitHub username',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Provide your email address',
+    },
+  ];
